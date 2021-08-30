d177e1222cf7a74bfdf271b46e9630a6ed700f52
Jerry Lin <jerryhaolin@gmail.com>
2021-08-30 12:10:43 -0400

    circle node 2 parallel print node index

---
+++ test/e2e/cypress/integration/localreviews/email_reviews5.spec.js
@@ -0,0 +1,25 @@
+describe( "LocalReviews - Connected Accounts", () => {
+  const base = require( "../../support/base" )
+  const local_reviews = require( "../../support/local_reviews" )
+  const dashboard = Cypress.env( "dashboard" )
+  const admin_panel = Cypress.env( "admin" )
+  const user_data = require( "../../fixtures/user_data" )
+
+  // beforeEach( () => {
+  //   const merchant_name = base.createMerchantName()
+  //   const dashboard_username = base.createRandomUsername()
+  //   base.login( admin_panel, "ac" )
+  //   base.deleteMerchantAndTwilioAccount()
+  //   local_reviews.createLocalReviewsMerchantAndDashboardUser( merchant_name, user_data.email, dashboard_username )
+  //   cy.get( "@merchant_id" )
+  //     .then( ( merchant_id ) => {
+  //       // local_messages.addLocalMessagesTwilioNumber( merchant_id )
+  //       // local_reviews.addPhoneNumber(merchant_id)
+  //     } )
+  // } )
+
+  it( "DSdssda", () => {
+    cy.log(process.env.CIRCLE_NODE_INDEX)
+    cy.wait(3000)
+  } )
+} )
