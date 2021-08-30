72e8d38e646b1160a396080fa463f9342b5d54c2
Jerry Lin <jerryhaolin@gmail.com>
2021-08-30 11:58:34 -0400

    circle node 2 parallel print node index

--- test/e2e/cypress/integration/localreviews/email_reviews2.spec.js
+++ test/e2e/cypress/integration/localreviews/email_reviews2.spec.js
@@ -1,16 +1,24 @@
-describe("dassad",()=>{
+describe( "LocalReviews - Connected Accounts", () => {
   const base = require( "../../support/base" )
-const user_data = require( "../../fixtures/user_data" )
-const local_reviews = require( "../../support/local_reviews" )
-const admin_panel = Cypress.env( "admin" )
-const dashboard = Cypress.env( "dashboard" )
-const dashboard_username = base.createRandomUsername()
-const merchant_name = base.createMerchantName()
+  const local_reviews = require( "../../support/local_reviews" )
+  const dashboard = Cypress.env( "dashboard" )
+  const admin_panel = Cypress.env( "admin" )
+  const user_data = require( "../../fixtures/user_data" )
 
+  // beforeEach( () => {
+  //   const merchant_name = base.createMerchantName()
+  //   const dashboard_username = base.createRandomUsername()
+  //   base.login( admin_panel, "ac" )
+  //   base.deleteMerchantAndTwilioAccount()
+  //   local_reviews.createLocalReviewsMerchantAndDashboardUser( merchant_name, user_data.email, dashboard_username )
+  //   cy.get( "@merchant_id" )
+  //     .then( ( merchant_id ) => {
+  //       // local_messages.addLocalMessagesTwilioNumber( merchant_id )
+  //       // local_reviews.addPhoneNumber(merchant_id)
+  //     } )
+  // } )
 
-  it("test",()=>{
-      base.login( admin_panel, "ac" )
-      // base.addTwilioNumber("6127c4f3cd6ce568e56d2351", "+14377476234")
-      base.removeTwilioNumber("6127c4f3cd6ce568e56d2351")
-  })
-})
+  it( "DSdssda", () => {
+    cy.log(process.env.CIRCLE_NODE_INDEX)
+  } )
+} )
--- test/e2e/cypress/integration/localreviews/email_reviews3.spec.js
+++ test/e2e/cypress/integration/localreviews/email_reviews3.spec.js
@@ -5,20 +5,20 @@ describe( "LocalReviews - Connected Accounts", () => {
   const admin_panel = Cypress.env( "admin" )
   const user_data = require( "../../fixtures/user_data" )
 
-  beforeEach( () => {
-    const merchant_name = base.createMerchantName()
-    const dashboard_username = base.createRandomUsername()
-    base.login( admin_panel, "ac" )
-    base.deleteMerchantAndTwilioAccount()
-    local_reviews.createLocalReviewsMerchantAndDashboardUser( merchant_name, user_data.email, dashboard_username )
-    cy.get( "@merchant_id" )
-      .then( ( merchant_id ) => {
-        // local_messages.addLocalMessagesTwilioNumber( merchant_id )
-        // local_reviews.addPhoneNumber(merchant_id)
-      } )
-  } )
+  // beforeEach( () => {
+  //   const merchant_name = base.createMerchantName()
+  //   const dashboard_username = base.createRandomUsername()
+  //   base.login( admin_panel, "ac" )
+  //   base.deleteMerchantAndTwilioAccount()
+  //   local_reviews.createLocalReviewsMerchantAndDashboardUser( merchant_name, user_data.email, dashboard_username )
+  //   cy.get( "@merchant_id" )
+  //     .then( ( merchant_id ) => {
+  //       // local_messages.addLocalMessagesTwilioNumber( merchant_id )
+  //       // local_reviews.addPhoneNumber(merchant_id)
+  //     } )
+  // } )
 
   it( "DSdssda", () => {
-    cy.log( "hello" )
+    cy.log(process.env.CIRCLE_NODE_INDEX)
   } )
 } )
