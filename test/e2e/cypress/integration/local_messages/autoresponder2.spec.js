ca55afcb9666d6b7382b20fb047a332986649532
Jerry Lin <jerryhaolin@gmail.com>
2021-09-10 16:04:27 -0400

    test

--- test/e2e/cypress/integration/local_messages/autoresponder2.spec.js
+++ test/e2e/cypress/integration/local_messages/autoresponder2.spec.js
@@ -1,29 +1,29 @@
-describe( "LocalMessages - Autoresponder", () => {
-  const base = require( "../../support/base" )
-  const local_messages = require( "../../support/local_messages" )
-  const admin_panel = Cypress.env( "admin" )
-  const dashboard = Cypress.env( "dashboard" )
-  const user_data = require( "../../fixtures/user_data" )
-  const dashboard_username = base.createRandomUsername()
-  const responder_text = `Sorry we are not available right now ${ Math.floor( Math.random() * 100000000 ) }`
-  // const phone_number = Cypress.config( "baseUrl" ).includes ("stage") ? "14377476336" : "14377472898"
-  // const merchant_name = "Test Automation Autoresponder"
+// describe( "LocalMessages - Autoresponder", () => {
+//   const base = require( "../../support/base" )
+//   const local_messages = require( "../../support/local_messages" )
+//   const admin_panel = Cypress.env( "admin" )
+//   const dashboard = Cypress.env( "dashboard" )
+//   const user_data = require( "../../fixtures/user_data" )
+//   const dashboard_username = base.createRandomUsername()
+//   const responder_text = `Sorry we are not available right now ${ Math.floor( Math.random() * 100000000 ) }`
+//   // const phone_number = Cypress.config( "baseUrl" ).includes ("stage") ? "14377476336" : "14377472898"
+//   // const merchant_name = "Test Automation Autoresponder"
 
 
-  it( "Should be able to enable autoresponder and receive autoresponder text", () => {
-    cy.log(Cypress.env("BORK"))//
-    base.login( admin_panel, "ac" )
-    base.removeTwilioNumber( "Test Automation" )
-    base.deleteMerchants( "Test Automation" )
-    base.removeTwilioNumber( "Test Automation 14377475919" )
-    base.deleteMerchants( "Test Automation 14377475919" )
-    base.removeTwilioNumber( "Test Automation 14377476738" )
-    base.deleteMerchants( "Test Automation 14377476738" )
-    base.removeTwilioNumber( "Test Automation 14377476234" )
-    base.deleteMerchants( "Test Automation 14377476234" )
-    base.removeTwilioNumber( "Test Automation 14377476589" )
-    base.deleteMerchants( "Test Automation 14377476589" )
-base.deleteIntercomUsers()
+//   it( "Should be able to enable autoresponder and receive autoresponder text", () => {
+//     cy.log(Cypress.env("BORK"))//
+//     base.login( admin_panel, "ac" )
+//     base.removeTwilioNumber( "Test Automation" )
+//     base.deleteMerchants( "Test Automation" )
+//     base.removeTwilioNumber( "Test Automation 14377475919" )
+//     base.deleteMerchants( "Test Automation 14377475919" )
+//     base.removeTwilioNumber( "Test Automation 14377476738" )
+//     base.deleteMerchants( "Test Automation 14377476738" )
+//     base.removeTwilioNumber( "Test Automation 14377476234" )
+//     base.deleteMerchants( "Test Automation 14377476234" )
+//     base.removeTwilioNumber( "Test Automation 14377476589" )
+//     base.deleteMerchants( "Test Automation 14377476589" )
+// base.deleteIntercomUsers()
 
-  } )
-} )
+//   } )
+// } )
