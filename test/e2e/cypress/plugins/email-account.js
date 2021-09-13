7b049902d1ee81b655f9de331062072eefe946dd
Jerry Lin <jerryhaolin@gmail.com>
2021-09-13 09:24:23 -0400

    test

--- test/e2e/cypress/plugins/email-account.js
+++ test/e2e/cypress/plugins/email-account.js
@@ -17,15 +17,29 @@ async function makeEmailAccount() {
 }
 
 async function getLastEmail( email_config, email_query ) {
+  console.log("&^$^%$^%$%^$&$%^^%$^$^&%$%^$^$%")
+  console.log("before connect")
+  console.log("&^$^%$^%$%^$&$%^^%$^$^&%$%^$^$%")
   const connection = await imaps.connect( email_config )
+  console.log("&^$^%$^%$%^$&$%^^%$^$^&%$%^$^$%")
+  console.log("after connect")
+  console.log("&^$^%$^%$%^$&$%^^%$^$^&%$%^$^$%")
   await connection.openBox( "INBOX" )
   const search_criteria = [ "UNSEEN", [ "SUBJECT", email_query ] ]
   const fetch_options = {
     bodies: [ "" ],
   }
   for( let count = 0 ; count < 15; count++ ) {
+    console.log("&^$^%$^%$%^$&$%^^%$^$^&%$%^$^$%")
+    console.log(count)
+    console.log("&^$^%$^%$%^$&$%^^%$^$^&%$%^$^$%")
+    console.log("&^$^%$^%$%^$&$%^^%$^$^&%$%^$^$%")
+    console.log("before search")
+    console.log("&^$^%$^%$%^$&$%^^%$^$^&%$%^$^$%")
     const messages = await connection.search( search_criteria, fetch_options )
-
+    console.log("&^$^%$^%$%^$&$%^^%$^$^&%$%^$^$%")
+    console.log("after search")
+    console.log("&^$^%$^%$%^$&$%^^%$^$^&%$%^$^$%")
     if( ! messages.length ) {
       console.log( "Cannot find any emails" )
     } else {
